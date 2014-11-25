510d329ce5b12f8f6203314a7a302816185b4472
firebroo <1121927992@qq.com>
2014-11-25 12:43:16 +0800

    Create problem4.py

---
+++ problem4/python/problem4.py
@@ -0,0 +1,15 @@
+def palindromicNumberList():
+    lis = []
+    for i in range(999*999,100*100,-1):
+        if i > 100000:
+            if str(i)[0:3] == (str(i)[::-1])[0:3]:
+                lis.append(i)
+        else:
+            break
+    return lis
+def isContain():
+    lis = palindromicNumberList()
+    for i in lis:
+        if i in [x*y for x in range(999,100,-1) for y in range(999,100,-1)]:
+            return i
+print isContain()
