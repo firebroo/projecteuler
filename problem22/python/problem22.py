67365ea31240f6e7f5835568211d1df109d13b12
firebroo <1121927992@qq.com>
2014-11-27 21:59:34 +0800

    Create problem22.py

---
+++ problem22/python/problem22.py
@@ -0,0 +1,22 @@
+def generateDictionary():
+    dic = {}
+    tmp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    lis2 = []
+    for i in tmp:
+        lis2.append(i)
+    dic = dict([(i,j) for (i,j) in zip(lis2,range(1,27))])
+    return dic
+
+def sumNamesScores():
+    dic = generateDictionary()
+    for i in open("p022_names.txt"):
+        lis = sorted(i.replace('"',"").split(','))
+        sumResult = 0
+        for (i,j) in zip(lis,range(1,len(lis)+1)):
+            tmp = 0
+            for k in i:
+                tmp += dic[k]
+            sumResult += tmp * j
+        return sumResult
+
+print sumNamesScores()
