f6a992cb00b23b63873ec253f6e1ccaef4e9604a
firebroo <1121927992@qq.com>
2014-12-07 22:51:24 +0800

    Create problem50.rb

---
+++ problem50/ruby/problem50.rb
@@ -0,0 +1,43 @@
+def isPrime(x)
+	high = Math.sqrt(x).to_i
+	for i in 2...(high+1)
+		if x % i == 0
+			return false
+		end
+	end
+	return true
+end
+
+def generateLimitAndPrimeList()
+	primesSum = 0     #素数和
+	counter = 0       #计数器
+	number = 2        #初始化为2
+	lis = []          #素数列表
+	while primesSum < 1000000
+		if isPrime(number)
+			primesSum += number
+			counter += 1
+			lis.push(number)
+		end
+		number += 1
+	end
+	return counter,lis
+end
+
+def main()
+	limit = generateLimitAndPrimeList()[0]
+	primeList = generateLimitAndPrimeList()[1]
+	limit.downto(1) do |i|
+		for j in 0..primeList.size
+			primesSum = primeList[j,j+i].inject(0) {|sum,i| sum+i}
+			if primesSum > 1000000
+				break
+			end
+			if isPrime(primesSum)
+				return primesSum
+			end
+		end
+	end
+end
+
+print main()
