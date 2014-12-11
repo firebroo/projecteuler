43460e33de5b0d6b8e6e8b981a4e548d286b0358
firebroo <1121927992@qq.com>
2014-12-11 14:45:40 +0800

    Update problem12.java

--- problem12/java/problem12.java
+++ problem12/java/problem12.java
@@ -1,62 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
-
-
-
-public class Demo {
-	public static void main(String[] args) {
-		Trangular trangular = new Trangular();
-		System.out.println(trangular.generate());
-	}
-
-}
-
-class Trangular{
-	public Integer generate() {
-		Integer tmp = 0;
-		Integer addNumber = 1;
-		while (true) {
-			tmp += addNumber;
-			addNumber++;
-			if (div(tmp).toArray().length > 50) {
+public class Problem12 {
+	public static Integer test() {
+		Integer tmp = 1;
+		for (Integer i = 2; i < 1000000; i++) {
+			tmp += i;
+			Integer counter = 0;
+			for (int j = 1; j < (int) Math.sqrt(tmp) + 1; j++) {
+				if (tmp % j == 0) {
+					counter += 1;
+				}
+			}
+			if (counter >= 250) {
 				return tmp;
 			}
 		}
+		return null;
 	}
-	
-	public  List<Integer> div(Integer x) {
-		List<Integer> list = new ArrayList<Integer>();
-		for (Integer i=1;i <= x; i++) {
-			if (x % i == 0) {
-				list.add(i);
-			}
-		}
-		return list;
-	}
-}
 
-
-
-public class Demo2 {
-	public static Integer test(Integer x) {
-		int result = 0;
-		for (int i = 1; i <= x; i++) {
-			if (x % i == 0) {
-				result += 1;
-			} else {
-				continue;
-			}
-		}
-		return result;
-	}
 	public static void main(String[] args) {
-		Integer tmp = 0;
-		for(Integer i=1;;i++) {
-			tmp += i;
-			if (test(tmp) > 500) {
-				System.out.println(tmp);
-				break;
-			}
-		}
+		System.out.println(test());
 	}
 }
