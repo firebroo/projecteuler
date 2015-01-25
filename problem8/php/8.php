33c1c7fe01fb77692216c51531943a2c07170c9f
firebroo <1121927992@qq.com>
2015-01-25 19:03:42 +0800

    Create 8.php

---
+++ problem8/php/8.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/1/25
+ * Time: 18:19
+ */
+function handle_file($file)
+{
+    $a = file($file);
+    $result = "";
+    foreach ($a as $line => $content) {
+        $result .= trim($content, "\r\n");
+    }
+    return $result;
+}
+
+function digit_mul($string)
+{
+    $mul = 1;
+    for ($j = 0; $j < strlen($string); $j++) {
+        $mul *= intval($string{$j});
+    }
+    return $mul;
+}
+
+$result = handle_file("./1.txt");
+$max = 0;
+for ($i = 0; $i < strlen($result) - 13; $i++) {
+    $sub_string = substr($result, $i, 13);
+    $mul = digit_mul($sub_string);
+    $max = $mul > $max ? $mul : $max;
+}
+echo $max;
+
