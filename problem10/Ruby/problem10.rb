a9aa14d772f09025ddd363898b2c4b1aa22f130a
firebroo <1121927992@qq.com>
2015-02-12 14:44:10 +0800

    Create problem10.rb

---
+++ problem10/Ruby/problem10.rb
@@ -0,0 +1,16 @@
+def isPrime(num)
+	2.upto(Math.sqrt(num)) do |i|
+		if num % i == 0
+			return false
+		end
+	end
+	return true
+end
+
+result = 0
+2.upto(2000000) do |i|
+	if isPrime(i)
+		result += i
+	end
+end
+puts result
