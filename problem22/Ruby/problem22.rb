ab41dfcc57c48c87c4e40bee9cdc671834305868
firebroo <1121927992@qq.com>
2015-02-13 23:27:18 +0800

    Create problem22.rb

---
+++ problem22/Ruby/problem22.rb
@@ -0,0 +1,31 @@
+def generateDict()
+	dict = {}
+	i = 1
+	('A'..'Z').each do |key|
+		dict[key] = i
+		i += 1
+	end
+	return dict
+end
+# 全局字典
+$dict = generateDict()
+def getRank(word)
+	rank = 0
+	word.each_char do |i|
+		rank += $dict[i]
+	end
+	return rank
+end
+
+file = File.open('1.txt','r')
+file.each do |i|
+	rank = 0
+	lis = i.gsub('"','').split(/,/).sort
+	helper = 1
+	lis.each do |j|
+		rank += getRank(j) * helper
+		helper = helper.succ
+	end
+	puts rank
+end
+file.close	
