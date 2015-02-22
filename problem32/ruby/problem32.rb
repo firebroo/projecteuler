2d98f1c8ce204957acef90b87e2b0a9d17f1065b
firebroo <1121927992@qq.com>
2015-02-22 15:30:04 +0800

    Update and rename problem32 to problem32/ruby/problem32.rb

---
+++ problem32/ruby/problem32.rb
@@ -0,0 +1,29 @@
+def isPandigital(x,y,z)
+	lis = []
+	result = x.to_s + y.to_s + z.to_s
+	if result.length == 9
+		result.each_char do |i|
+			lis.push(i.to_i)
+		end
+		lis.sort!
+		if lis == [1,2,3,4,5,6,7,8,9]
+			return true
+		end
+	end
+	return false
+end
+
+def main
+	lis = []
+	(1..5000).each do |i|
+		(1..5000).each do |j|
+			product = i * j
+			if isPandigital(i, j, product)
+				lis.push(product)
+			end
+		end
+	end
+	lis.uniq
+end
+
+puts main.inject {|i, sum| sum += i}
