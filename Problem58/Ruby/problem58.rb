c06a47d1dc16b388745d10330d633d9ffed99aed
firebroo <1121927992@qq.com>
2015-02-27 14:10:02 +0800

    Update problem58.rb

--- Problem58/Ruby/problem58.rb
+++ Problem58/Ruby/problem58.rb
@@ -1,40 +1,24 @@
-def generator()
-  init = 1
-  list = [init]
-  step = 2
-  primeList = []
-  count = 1
-  while true do
-    nextNumber = init + step
-    list.push(nextNumber)
-    if isPrime(nextNumber)
-      primeList.push(nextNumber)
-    end
-    3.times do
-      nextNumber += step
-      if isPrime(nextNumber)
-        primeList.push(nextNumber)
-      end
-      list.push(nextNumber)
-    end
-    step += 2
-    init = nextNumber
-    count += 1
-    if primeList.length.to_f / list.length.to_f < 0.1
-      return count
-    end
-  end
-end
-
-
 def isPrime(number)
   (2..Math.sqrt(number)+1).each do |i|
-    if number % i == 0
-      return false
-    end
+    return false if number % i == 0
   end
   return true
 end
 
-p generator * 2 - 1
+def main()
+  listCount = 1
+  step = 2
+  primeCount = 0
+  nextNumber = 1
+  while true do
+    4.times do
+      nextNumber += step
+      primeCount += 1 if isPrime(nextNumber)
+    end
+    listCount += 4
+    step += 2
+    return step - 1 if primeCount.to_f / listCount.to_f < 0.1
+  end
+end
 
+p main
