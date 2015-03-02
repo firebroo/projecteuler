b9743fa136def5b7302f96c9b2cbaaf8b5fd08f2
firebroo <1121927992@qq.com>
2015-03-02 20:01:58 +0800

    Update problem70.rb

--- Problem70/Ruby/problem70.rb
+++ Problem70/Ruby/problem70.rb
@@ -63,44 +63,5 @@ def DigitsIsSame(num1, num2)
   return stringToList(num1) == stringToList(num2)
 end
 
-def getPrimeNum(num)
-  divisors = []
-  (2..num/2).each do |i|
-    if num % i == 0
-      divisors.push(i)
-    end
-  end
-  count = []
-  divisors.each do |j|
-    (1..100000000).each do |k|
-      tmp = j*k
-      if tmp >= num
-        break
-      else
-        count.push(tmp)
-      end
-    end
-  end
-  return num - count.uniq.length - 1
-end
 
-
-#p getPrimeNum(87109)
-
-=begin
-min = 100
-num = 0
-(10**7).downto(10**6).each do |i|
-  if isPrime(i)
-    #p i
-    tmp = i / getPrimeNum(i)
-    if tmp < min
-      min = tmp
-      num = i
-    end
-  end
-end
-
-p num
-=end
 
