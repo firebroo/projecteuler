b0086a7a852ad5ad5efc972a7ffc9a24506fa476
firebroo <1121927992@qq.com>
2015-03-20 11:43:25 +0800

    Update problem84.java

--- problem87/java/problem84.java
+++ problem87/java/problem84.java
@@ -19,11 +19,11 @@ public class Problem87 {
 
     private static List<Integer> getPrimeList(int num) {
         List<Integer> list = new ArrayList<>();
-        int tmp = (int)Math.sqrt(num);
-        for(int i=2;i<tmp+1;i++) {
-           if (isPrime(i)) {
-              list.add(i);
-           }
+        int tmp = (int) Math.sqrt(num);
+        for (int i = 2; i < tmp + 1; i++) {
+            if (isPrime(i)) {
+                list.add(i);
+            }
         }
         return list;
     }
@@ -32,16 +32,16 @@ public class Problem87 {
         long start = System.currentTimeMillis();
         List<Integer> list = getPrimeList(50000000);
         Set<Integer> set = new HashSet<>();
-        for(Integer i : list) {
-            int tmp1 = (int)Math.pow(i,2);
-            for(Integer j : list) {
-                int tmp2 = (int)Math.pow(j,3);
+        for (Integer i : list) {
+            int tmp1 = (int) Math.pow(i, 2);
+            for (Integer j : list) {
+                int tmp2 = (int) Math.pow(j, 3);
                 int sum1 = tmp1 + tmp2;
                 if (sum1 >= 50000000) break;
-                for(Integer k : list) {
-                  int tmp3 = (int)Math.pow(k,4);
+                for (Integer k : list) {
+                    int tmp3 = (int) Math.pow(k, 4);
                     int sum2 = sum1 + tmp3;
-                    if(sum2>=50000000) break;
+                    if (sum2 >= 50000000) break;
                     set.add(sum2);
                 }
             }
